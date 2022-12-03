4538f52c16f260f4148e90734555a162839bf2e5
Mateus Lourenço <mateuslourenco55@outlook.com>
2022-12-03 18:43:49 -0300

    lista01: ex02

--- lista-01/js02-metros-para-centimetros.js
+++ lista-01/js02-metros-para-centimetros.js
@@ -15,10 +15,10 @@
  */
 
 // Entrada
-var metros = 1
+var metros = .5
 
 // Altere a expressão abaixo para calcular cm com base na var metros
-var cm = 100
+var cm = metros * 100
 
 var resultado = '👉 ' + metros + ' metro(s) == ' + cm + ' centímetro(s)'
 
