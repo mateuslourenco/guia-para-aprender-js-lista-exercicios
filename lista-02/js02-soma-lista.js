9a7e55f6810c26ae740a7ffaf3726fc1ed31ca1d
Mateus Lourenço <mateuslourenco55@outlook.com>
2022-12-03 20:27:18 -0300

    lista02: js02

--- lista-02/js02-soma-lista.js
+++ lista-02/js02-soma-lista.js
@@ -3,14 +3,16 @@
  * 
  * ENTRADA              SAÍDA
  * [1,3,5,7,8,10]       34
- * [1,1,40]             40
+ * [1,1,40]             42
  */
 
 // Entrada
-var entrada = [1,3,5,7,8,10]
+var entrada = [1,1,40]
 
 var soma = 0
 
 // percorrer a lista e fazer a somatória dos elementos
-
+entrada.forEach(numero => {
+    soma += numero
+})
 console.log(soma)
